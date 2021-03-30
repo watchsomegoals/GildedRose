1ee70ebb3d496d697f5e9f74aabbb0672e8567ca
Valentin Caraba <if18b122@technikum-wien.at>
2021-03-30 17:17:08 +0200

    Initialized project

---
+++ GildedRose/GildedRose/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GildedRose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
